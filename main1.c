c7f59451937c64d2a31ff5f7bd5026aee7327672
hyerin012 <70803588+hyerin012@users.noreply.github.com>
2020-11-09 00:51:58 +0900

    04 exercise

--- main1.c
+++ main1.c
@@ -3,14 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void swap(int *x,int *y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
+
+
+
+
 int main(int argc, char *argv[]) {
 	
 	int a=3;
 	int b=5;
 	
-	swap(a,b);
+	swap(&a,&b);
 	printf("a:%i,b:%i\n",a,b);
 	
-	
-	return 0;
+		return 0;
 }
